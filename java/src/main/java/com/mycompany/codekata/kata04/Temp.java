b5cd1ec1b858e4dfbf078df8a99c3209cb8313f3
notapresent <notapresent@gmail.com>
2017-05-24 23:44:23 +0300

    kata 04 - part 1 - java (wip)

--- java/src/main/java/com/mycompany/codekata/kata04/Temp.java
+++ java/src/main/java/com/mycompany/codekata/kata04/Temp.java
@@ -2,4 +2,24 @@ package com.mycompany.codekata.kata04;
 
 
 public class Temp {
+    private String filename;
+    public Temp(String filename) {
+        this.filename = filename;
+    }
+
+    public static void main(String[] args) {
+        String filename;
+        if (args.length > 0) {
+            filename = args[0];
+        } else {
+            filename = "weather.dat";
+        }
+
+        Temp minSpreadCalculator = new Temp(filename);
+        System.out.println(minSpreadCalculator.result());
+    }
+
+    int result() {
+        return -1;
+    }
 }
